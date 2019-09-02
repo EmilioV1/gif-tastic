54f88d63bbf646ccc6620a2f9ed50ab87f70a064
Emilio Valladares <vallae.wumbo@gmail.com>
2019-09-02 00:03:59 -0700

    Function to pop buttons created.

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+$(function(){
+    console.log("page loaded");
+})
+
+var searchArray = ['dog', 'cat', 'bird'];
+
+function populateButtons(searchArray, classToAdd, areaToAddTo){
+    $(areaToAddTo).empty();
+    for(var i = 0; i < searchArray.length; i++){
+         
+    }
+}
