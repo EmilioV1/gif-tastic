0337560a6824fdf30cfef08f36e96ffe946976a9
Emilio Valladares <vallae.wumbo@gmail.com>
2019-09-05 00:27:06 -0700

    Query URL change from http to https to prevent any errors on deployed page.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,7 +22,7 @@ function populateButtons(searchArray, classToAdd, areaToAddTo){
 $(document).on('click', '.searchButton', function(){
     $('#searches').empty();
     var type = $(this).data('type');
-    var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+type+'&api_key=uQIcYcgq3RPyde5FOKWYggxoWS7A9hFN&limit=10'
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?q='+type+'&api_key=uQIcYcgq3RPyde5FOKWYggxoWS7A9hFN&limit=10'
     $.ajax({url: queryURL, method: 'GET'})
         .done(function(response){
             for(var i=0; i<response.data.length; i++){
