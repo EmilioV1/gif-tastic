58ad711287261fec21f7550cdb1bee7bddb2ce30
Emilio Valladares <vallae.wumbo@gmail.com>
2019-09-04 20:40:38 -0700

    Function to collect data from giphy api and append gif to page.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,9 +3,10 @@ $(function(){
     console.log("page loaded");
 })
 
+
 var searchArray = ['dog', 'cat', 'bird'];
 
-// 
+// Populates buttons to page
 function populateButtons(searchArray, classToAdd, areaToAddTo){
     $(areaToAddTo).empty();
     for(var i = 0; i < searchArray.length; i++){
@@ -16,3 +17,28 @@ function populateButtons(searchArray, classToAdd, areaToAddTo){
          $(areaToAddTo).append(a);
     }
 }
+
+// Hits giphy api for data, collects still, collects animated url and appends gif
+$(document).on('click', '.searchButton', function(){
+    var type = $(this).data('type');
+    var queryURL = 'http://api.giphy.com/v1/gifs/search?q='+type+'&api_key=uQIcYcgq3RPyde5FOKWYggxoWS7A9hFN&limit=10'
+    $.ajax({url: queryURL, method: 'GET'})
+        .done(function(response){
+            for(var i=0; i<response.data.length; i++){
+                var searcgDiv = $('<div class="search-item">');
+                var rating = response.data[i].rating;
+                var p = $('<p>').text('rating: ' + rating);
+                var animated = response.data[i].images.fixed_height.url;
+                var still = response.data[i].images.fixed_height_still.url;
+                var image = $('<img>');
+                image.attr('src', still);
+                image.attr('data-still', still);
+                image.attr('data-animated', animated);
+                image.attr('data-state', 'still');
+                image.addClass('searchImage');
+                searchDiv.append(p);
+                searchDiv.append(image);
+                $('#searches').append(searchDiv);
+            }        
+        })
+})
